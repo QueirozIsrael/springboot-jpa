f2d9f5613867d84dc76a41f8df8f7be2aca1c56a
Israel Queiroz <israel.qoliveira@gmail.com>
2025-01-07 13:12:00 -0300

    User entity and resource

--- src/main/java/com/queirozisrael/springBoot/Application.java
+++ src/main/java/com/queirozisrael/springBoot/Application.java
@@ -3,7 +3,7 @@ package com.queirozisrael.springBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "resources")
 public class Application {
 
 	public static void main(String[] args) {
