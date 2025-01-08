3d7e49b4d3e25df30323a3db4bfda304151b801d
Israel Queiroz <israel.qoliveira@gmail.com>
2025-01-07 22:22:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/queirozisrael/springBoot/Application.java
+++ src/main/java/com/queirozisrael/springBoot/Application.java
@@ -2,8 +2,10 @@ package com.queirozisrael.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "resources")
+@EntityScan(basePackages = "entities")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,12 +1,19 @@
 package entities;
 
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
