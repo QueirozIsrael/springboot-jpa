2c22ae6b17374aa53a0a80f92b9a7cc4d0977c65
Israel Queiroz <israel.qoliveira@gmail.com>
2025-01-17 11:00:36 -0300

    ExceptionHandling - findById

--- src/main/java/com/queirozisrael/springBoot/services/UserService.java
+++ src/main/java/com/queirozisrael/springBoot/services/UserService.java
@@ -2,6 +2,7 @@ package com.queirozisrael.springBoot.services;
 
 import com.queirozisrael.springBoot.entities.User;
 import com.queirozisrael.springBoot.repositories.UserRepository;
+import com.queirozisrael.springBoot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findByID(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
